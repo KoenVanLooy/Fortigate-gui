7ae12be49a225fd629b4e763da9363806f8c9539
Koen Van Looy <r0444972@student.thomasmore.be>
2021-02-17 20:39:55 +0100

    Checkup Database

--- Fortigate-Gui/Data/Migrations/20210217192001_InitialCreate.Designer.cs
+++ Fortigate-Gui/Data/Migrations/20210217192001_InitialCreate.Designer.cs
@@ -4,14 +4,16 @@ using Fortigate_Gui.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Fortigate_Gui.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210217192001_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
